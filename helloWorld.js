09cdecdaa66a510545f008d3aa7ebc80c12bfdc6
notsoarsh <arshpalwork@gmail.com>
2025-08-16 00:48:42 +0530

    First Commit

---
+++ helloWorld.js
@@ -0,0 +1,5 @@
+//this is the first code for the basics
+//lets start off things with this classic
+console.log("Hello World");
+//console - is an object reference that lets you access the debugging console
+// log -  is a function
